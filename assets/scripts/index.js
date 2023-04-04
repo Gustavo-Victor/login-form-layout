b6357d72b0187a9a47cfaaff9390418edf3b74e5
Gustavo Victor <gguvictor909@gmail.com>
2023-04-04 10:21:01 -0300

    Finish project

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,11 +1,19 @@
+//elements
 const loginForm = document.querySelector("#login-form"); 
 const inputEmail = document.querySelector("#email");
 const inputPassword = document.querySelector("#password");
+const inputCheckbox = document.querySelector("#remember"); 
 const eyeIcon = document.querySelector("#eye");
+const inputs = document.querySelectorAll(".validate"); 
 
 
+//events
 loginForm.addEventListener("submit", (e) => {
-    e.preventDefault();
+    e.preventDefault();    
+    window.alert('Thank you!'); 
+    inputEmail.value = ""; 
+    inputPassword.value = ""; 
+    window.location.href = './'; 
 }); 
 
 eyeIcon.addEventListener("click", () => {
@@ -14,5 +22,4 @@ eyeIcon.addEventListener("click", () => {
     } else {
         inputPassword.setAttribute("type", "password")
     }
-})
-
+}); 
